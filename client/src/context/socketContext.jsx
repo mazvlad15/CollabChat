119c4c481cbffbcb9667b6f542b9897c3d5617c3
mazvlad15 <vlad.mazureac2003@gmail.com>
2024-12-05 18:54:51 +0200

    Add new link for io

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authState) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://collabchat.onrender.com", {
         query: {
           userId: authState._id,
         },
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://collabchat.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH"],
   },
 });
